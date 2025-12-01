1c92a5080c89f7c2328502ebf0910b2802479032
HarshiniAiyyer <kerneldacore@gmail.com>
2025-12-01 21:14:31 +0530

    updated api

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from typing import Optional, List
 
 
 class ChatRequest(BaseModel):
-    message: str = Field(..., description="User question")
+    message: Optional[str] = Field(default="", description="User question")
     conversation_id: Optional[str] = None
     max_tokens: int = 512
     temperature: float = 0.1
